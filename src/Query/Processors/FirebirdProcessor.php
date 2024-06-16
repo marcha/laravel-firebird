5e1c262be393bcd6a236fdc2f3ab31eb9244d030
marcha <marcic74@gmail.com>
2024-06-16 13:39:01 +0200

    compileColumns processColumns

--- src/Query/Processors/FirebirdProcessor.php
+++ src/Query/Processors/FirebirdProcessor.php
@@ -8,91 +8,123 @@ use Illuminate\Database\Query\Processors\Processor;
 class FirebirdProcessor extends Processor
 {
 
-    /**
-     * Process an "insert get ID" query.
-     *
-     * @param \Illuminate\Database\Query\Builder $query
-     * @param string $sql
-     * @param array $values
-     * @param string $sequence
-     * @return int
-     */
-    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
-    {
-        $results = $query->getConnection()->selectFromWriteConnection($sql, $values);
+  /**
+   * Process an "insert get ID" query.
+   *
+   * @param \Illuminate\Database\Query\Builder $query
+   * @param string $sql
+   * @param array $values
+   * @param string $sequence
+   * @return int
+   */
+  public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+  {
+    $results = $query->getConnection()->selectFromWriteConnection($sql, $values);
 
-        $sequence = $sequence ?: 'ID';
+    $sequence = $sequence ?: 'ID';
 
-        $result = (array)$results[0];
+    $result = (array)$results[0];
 
-        $id = $result[$sequence];
+    $id = $result[$sequence];
 
-        return is_numeric($id) ? (int)$id : $id;
-    }
+    return is_numeric($id) ? (int)$id : $id;
+  }
 
-    /**
-     * Process an "next sequence value" query.
-     *
-     * @param \Illuminate\Database\Query\Builder $query
-     * @param string $sql
-     * @return int
-     */
-    public function processNextSequenceValue(Builder $query, $sql)
-    {
-        $results = $query->getConnection()->selectFromWriteConnection($sql);
+  /**
+   * Process an "next sequence value" query.
+   *
+   * @param \Illuminate\Database\Query\Builder $query
+   * @param string $sql
+   * @return int
+   */
+  public function processNextSequenceValue(Builder $query, $sql)
+  {
+    $results = $query->getConnection()->selectFromWriteConnection($sql);
 
-        $result = (array)$results[0];
+    $result = (array)$results[0];
 
-        $id = $result['ID'];
+    $id = $result['ID'];
 
-        return is_numeric($id) ? (int)$id : $id;
-    }
+    return is_numeric($id) ? (int)$id : $id;
+  }
 
-    /**
-     * Process an "get context variable value" query.
-     *
-     * @param \Illuminate\Database\Query\Builder $query
-     * @param string $sql
-     * @return int
-     */
-    public function processGetContextValue(Builder $query, $sql)
-    {
-        $result = $query->getConnection()->selectOne($sql);
+  /**
+   * Process an "get context variable value" query.
+   *
+   * @param \Illuminate\Database\Query\Builder $query
+   * @param string $sql
+   * @return int
+   */
+  public function processGetContextValue(Builder $query, $sql)
+  {
+    $result = $query->getConnection()->selectOne($sql);
 
-        return $result['VAL'];
-    }
+    return $result['VAL'];
+  }
 
-    /**
-     * Process an "execute function" query.
-     *
-     * @param \Illuminate\Database\Query\Builder $query
-     * @param string $sql
-     * @param array $values
-     *
-     * @return mixed
-     */
-    public function processExecuteFunction(Builder $query, $sql, $values)
-    {
-        $result = $query->getConnection()->selectOne($sql, $values);
+  /**
+   * Process an "execute function" query.
+   *
+   * @param \Illuminate\Database\Query\Builder $query
+   * @param string $sql
+   * @param array $values
+   *
+   * @return mixed
+   */
+  public function processExecuteFunction(Builder $query, $sql, $values)
+  {
+    $result = $query->getConnection()->selectOne($sql, $values);
 
-        return $result['VAL'];
-    }
+    return $result['VAL'];
+  }
 
-    /**
-     * Process the results of a column listing query.
-     *
-     * @param array $results
-     * @return array
-     */
-    public function processColumnListing($results)
-    {
-        $mapping = function ($r) {
-            $r = (object)$r;
+  /**
+   * Process the results of a column listing query.
+   *
+   * @param array $results
+   * @return array
+   */
+  public function processColumnListing($results)
+  {
+    $mapping = function ($r) {
+      $r = (object)$r;
 
-            return trim($r->{'RDB$FIELD_NAME'});
-        };
+      return trim($r->{'RDB$FIELD_NAME'});
+    };
 
-        return array_map($mapping, $results);
-    }
+    return array_map($mapping, $results);
+  }
 
+
+  /**
+   * Process the results of a columns query.
+   *
+   * @param  array  $results
+   * @return array
+   */
+  public function processColumns($results)
+  {
+    return array_map(function ($result) {
+      $result = (object) $result;
+
+      return [
+        'name' => trim($result->name),
+        'type_name' => $result->type_name,
+        'type' => $result->type,
+        'collation' => $result->collation,
+        'nullable' => $result->nullable === 'YES',
+        'default' => $result->default,
+        'auto_increment' => $result->extra === 'auto_increment',
+        'comment' => $result->comment ?: null,
+        'generation' => $result->expression ? [
+          'type' => match ($result->extra) {
+            'STORED GENERATED' => 'stored',
+            'VIRTUAL GENERATED' => 'virtual',
+            default => null,
+          },
+          'expression' => $result->expression,
+        ] : null,
+      ];
+    }, $results);
+  }
 }
